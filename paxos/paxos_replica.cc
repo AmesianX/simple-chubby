aab828e2a6b2080e8619638eb62aa8428984b954
Hang Qu <quhang@stanford.edu>
2014-11-28 17:20:34 -0800

    Refactor the code.

--- paxos/paxos_replica.cc
+++ paxos/paxos_replica.cc
@@ -1,7 +1,9 @@
 #include <unistd.h>
 #include <sys/socket.h>
 
+#include <exception>
 #include <iostream>
+#include <string>
 #include <thread>
 
 #include <xdrpp/server.h>
@@ -9,11 +11,26 @@
 #include <xdrpp/socket.h>
 
 #include "paxos/paxos.hh"
+#include "paxos/paxos_impl.hh"
+
+void paxos_listener_thread_entry(const std::string& port) {
+  xdr::rpc_tcp_listener paxos_listener(
+      xdr::tcp_listen(port.c_str(), AF_INET));
+  paxos_v1_server paxos_server;
+  try {
+    paxos_listener.register_service(paxos_server);
+    paxos_listener.run();
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
+// auto fd = xdr::tcp_connect(host_port.c_str(), listening_port.c_str());
+// auto* client = new xdr::srpc_client<paxos_v1>{fd.release()};
 
 int main(int argc, char* argv[]) {
-
-//xdr::rpc_tcp_listener paxos_in(tcp_listen(listening_port, AF_INET));
-
-
+  std::string listening_port("19899");
+  paxos_listener_thread_entry(listening_port);
+  std::cerr << "ERROR: listener thread exits!" << std::endl;
   return 0;
 }
