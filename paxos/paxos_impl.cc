8469cfde0e5d5f00bdbe8324aefa542b127a617c
Hang Qu <quhang@stanford.edu>
2014-12-02 10:21:52 -0800

    Update the implementation file, fresh.

--- paxos/paxos_impl.cc
+++ paxos/paxos_impl.cc
@@ -3,16 +3,6 @@
 
 #include "paxos/paxos_impl.hh"
 
-std::unique_ptr<execute_res>
-paxos_v1_server::execute(std::unique_ptr<execute_arg> arg)
-{
-  std::unique_ptr<execute_res> res(new execute_res);
-  
-  // Fill in function body here
-  
-  return res;
-}
-
 std::unique_ptr<replicate_res>
 paxos_v1_server::replicate(std::unique_ptr<replicate_arg> arg)
 {
@@ -47,6 +37,16 @@ std::unique_ptr<init_view_res>
 paxos_v1_server::init_view(std::unique_ptr<init_view_arg> arg)
 {
   std::unique_ptr<init_view_res> res(new init_view_res);
+  
+  // Fill in function body here
+  
+  return res;
+}
+
+std::unique_ptr<execute_res>
+paxo_client_v1_server::execute(std::unique_ptr<execute_arg> arg)
+{
+  std::unique_ptr<execute_res> res(new execute_res);
   
   // Fill in function body here
   
--- paxos/paxos_impl.hh
+++ paxos/paxos_impl.hh
@@ -11,11 +11,17 @@ class paxos_v1_server {
 public:
   using rpc_interface_type = paxos_v1;
 
-  std::unique_ptr<execute_res> execute(std::unique_ptr<execute_arg> arg);
   std::unique_ptr<replicate_res> replicate(std::unique_ptr<replicate_arg> arg);
   std::unique_ptr<view_change_res> view_change(std::unique_ptr<view_change_arg> arg);
   std::unique_ptr<new_view_res> new_view(std::unique_ptr<new_view_arg> arg);
   std::unique_ptr<init_view_res> init_view(std::unique_ptr<init_view_arg> arg);
+};
+
+class paxo_client_v1_server {
+public:
+  using rpc_interface_type = paxo_client_v1;
+
+  std::unique_ptr<execute_res> execute(std::unique_ptr<execute_arg> arg);
 };
 
 #endif // !__XDR_PAXOS_PAXOS_IMPL_HH_INCLUDED__
