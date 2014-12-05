b2a48555f86d6a4407c54d5fc3b3079f1a2e2219
Hang Qu <quhang@stanford.edu>
2014-12-04 18:49:35 -0800

    Script for drawing workflow.

---
+++ makefig/app_1.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+import matplotlib.pyplot as plt
+
+def DrawArrowFront(start_time, duration, name, start_event, end_event):
+    plt.arrow(left_line,top_line-start_time,line_delta,-duration,
+              head_width=0.1,head_length=0.1,length_includes_head=True,color='k')
+    if name:
+        plt.annotate(name,
+                     xy=(left_line+line_delta/2,
+                         text_float+top_line-start_time-duration/2),
+                     horizontalalignment='center')
+    if start_event:
+        plt.annotate(start_event,
+                     xy=(left_line,top_line-start_time),
+                     xytext=(left_line-text_width,top_line-start_time),
+                     horizontalalignment='left',
+                     arrowprops=dict(arrowstyle="-",color="k",linestyle='dashed'))
+    if end_event:
+        plt.annotate(end_event,
+                     xy=(right_line,top_line-start_time-duration),
+                     xytext=(right_line+text_width,top_line-start_time-duration),
+                     horizontalalignment='right',
+                     arrowprops=dict(arrowstyle="-",color="k",linestyle='dashed'))
+
+def DrawArrowBack(start_time, duration, name, start_event, end_event):
+    plt.arrow(right_line, top_line-start_time,-line_delta,-duration,
+              head_width=0.1,head_length=0.1,length_includes_head=True,color='k')
+    if name:
+        plt.annotate(name,
+                     xy=(left_line+line_delta/2,
+                         text_float+top_line-start_time-duration/2),
+                     horizontalalignment='center')
+    if start_event:
+        plt.annotate(start_event,
+                     xy=(right_line,top_line-start_time),
+                     xytext=(right_line+text_width,top_line-start_time),
+                     horizontalalignment='right',
+                     arrowprops=dict(arrowstyle="-",color="k",linestyle='dashed'))
+    if end_event:
+        plt.annotate(end_event,
+                     xy=(left_line,top_line-start_time-duration),
+                     xytext=(left_line-text_width,top_line-start_time-duration),
+                     horizontalalignment='left',
+                     arrowprops=dict(arrowstyle="-",color="k",linestyle='dashed'))
+
+
+text_float = 0.1
+line_delta = 0.5
+bottom_line = 0
+top_line = 10
+text_width = 0.3
+left_right_margin = 0.5
+
+left_line = 0
+right_line = 0.5
+drift = 5
+plt.annotate('c0',xy=(0, 10.2),horizontalalignment='center')
+plt.annotate('s',xy=(0.5, 10.2),horizontalalignment='center')
+plt.annotate('c1',xy=(1, 10.2),horizontalalignment='center')
+plt.plot([left_line]*2,[bottom_line+drift,top_line],'k',linewidth=6)
+plt.annotate('client failure',
+             xy=(left_line,
+                 bottom_line+drift),
+             xytext=(left_line-0.5,
+                 bottom_line+drift),
+             horizontalalignment='left',
+             arrowprops=dict(arrowstyle="-",color="k",linestyle='dashed'))
+plt.plot([right_line]*2,[bottom_line,top_line],'k',linewidth=6)
+
+DrawArrowFront(1, 0.1, 'A1', 'a1', '')
+DrawArrowBack(1.5, 0.1, 'A2', '', 'a2')
+DrawArrowFront(2, 0.1, 'A3', 'a3', '')
+
+left_line = 0.5
+right_line = 1
+plt.plot([right_line]*2,[bottom_line,top_line],'k',linewidth=6)
+DrawArrowFront(2.1, 0.1, 'B1', '', 'b1')
+DrawArrowBack(2.6, 0.1, 'B2', 'b2', '')
+
+plt.axis('off')
+left_line = 0
+right_line = 1
+plt.xlim(left_line-text_width-left_right_margin,
+         right_line+text_width+left_right_margin)
+plt.ylim(bottom_line, top_line+0.5)
+plt.show();
